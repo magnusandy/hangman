b8710771948f3ad7feba4967d9a8b8bf2a5119ea
Andrew Magnus <magnusandy@gmail.com>
2016-08-08 21:05:01 -0600

    playing around with atom and made some changes

--- js/hangmanJS.js
+++ js/hangmanJS.js
@@ -14,8 +14,8 @@ var usedLetters = [];
     var valid = validateUserInput(puzzleText);
     if(puzzleText == "")//if there is nothing in the box, clear the generated link box
     {
-	document.getElementById("generatedLink").value = ""
-        return
+      document.getElementById("generatedLink").value = ""
+      return
     }
     if(valid)//the string contains only letters
     {
@@ -24,7 +24,7 @@ var usedLetters = [];
       if (warning.className.indexOf("w3-hide") == -1)//If the warning is NOT hidden, then hide it
       {
         warning.className += " w3-hide";//hide the warning
-      } 
+      }
       cleanPuzzle = cleanPuzzleString(puzzleText);
       addGeneratedLinkToPage(cleanPuzzle)
     }
@@ -35,8 +35,8 @@ var usedLetters = [];
     }
   }
     /*
-  Validates the submitted string by checking to make sure that the characters of the string are 
-  only letters (a-z, A-Z) or spaces. 
+  Validates the submitted string by checking to make sure that the characters of the string are
+  only letters (a-z, A-Z) or spaces.
   */
   function validateUserInput(userInput)
   {
@@ -52,7 +52,7 @@ var usedLetters = [];
     }
     return true
   }
-  
+
   /*
     Cleanse the input string, trim away leading/trailing spaces as well as change all to UPPERCASE
   */
@@ -62,7 +62,7 @@ var usedLetters = [];
    puzzleString = puzzleString.trim();
    return puzzleString;
   }
-  
+
   /*
   Based on the given string "puzzleText" generate a base64 encoded value and use it to create a puzzle link, return the link
   */
@@ -74,57 +74,57 @@ var usedLetters = [];
     var puzzleLink = "http://magnusandy.github.io/hangman/puzzle.html?string="+encodedPuzzle;
     return puzzleLink
   }
-  
+
   /*Add a generated puzzle link to the link textbox*/
   function addGeneratedLinkToPage(puzzleText)
   {
-	puzzleLink = generateLink(puzzleText)
-	document.getElementById("generatedLink").value = puzzleLink;
+    puzzleLink = generateLink(puzzleText)
+    document.getElementById("generatedLink").value = puzzleLink;
   }
-  
+
   /*
-	Highlight the text in the generated link field and copy to clipboard
+    Highlight the text in the generated link field and copy to clipboard
   */
   function pressCopyButton()
   {
-	var copyTextarea = document.getElementById("generatedLink");
-	copyTextarea.select();
-	document.execCommand("copy");
+    var copyTextarea = document.getElementById("generatedLink");
+    copyTextarea.select();
+    document.execCommand("copy");
   }
-  
+
   /*
-	Direct the user to the puzzle page for their generated game
+    Direct the user to the puzzle page for their generated game
   */
   function pressGeneratedPlayButton()
   {
-	  var link = document.getElementById("generatedLink").value;
-	  if(link != "")
-	  {
-		location.href=link;
-	  }
+      var link = document.getElementById("generatedLink").value;
+      if(link != "")
+      {
+        location.href=link;
+      }
   }
   /*
-	pull a random phrase and make it for play
+    pull a random phrase and make it for play
   */
   function pullRandomPhrase()
   {
-	randomNum = Math.floor((Math.random() * 3608));
-	return phrases[randomNum].word
+    randomNum = Math.floor((Math.random() * 3608));
+    return phrases[randomNum].word
   }
-  
+
   /*pulls the user into a randomly generated game when button is pressed*/
   function playRandomGame()
   {
-	var phrase = pullRandomPhrase()
-	link = generateLink(phrase)
-	location.href=link;
+    var phrase = pullRandomPhrase()
+    link = generateLink(phrase)
+    location.href=link;
   }
 
 /*add functionality so that when enter is pressed on the phrase box, the generate button is pressed*/
 function onEnterGenerate(event)
 {
     if (event.keyCode == 13)
-    { 
+    {
         pressGenerateButton();
     }
 }
@@ -137,32 +137,32 @@ function onEnterGenerate(event)
 /*
 used to grab the base64 encoded value from the url of the page to create the puzzle
 */
-	function getQueryVariable(variable)
-	{
-		   var query = window.location.search.substring(1);
-		   var vars = query.split("&");
-		   for (var i=0;i<vars.length;i) {
-				   var pair = vars[i].split("=");
-				   if(pair[0] == variable){return pair[1];}
-		   }
-		   return(false);
-	}
-	
-	/*decodes the puzzlestring from the url and returns it*/
-	function getFullPuzzleString()
-	{
-		var x = getQueryVariable("string")//get the value from the url
-		var decode = window.atob(x)//decode value from url
-		return decode
-	}
-	
+    function getQueryVariable(variable)
+    {
+           var query = window.location.search.substring(1);
+           var vars = query.split("&");
+           for (var i=0;i<vars.length;i) {
+                   var pair = vars[i].split("=");
+                   if(pair[0] == variable){return pair[1];}
+           }
+           return(false);
+    }
+
+    /*decodes the puzzlestring from the url and returns it*/
+    function getFullPuzzleString()
+    {
+        var x = getQueryVariable("string")//get the value from the url
+        var decode = window.atob(x)//decode value from url
+        return decode
+    }
+
 
     /*
-        Initialize the visibleArray, setting values to true for spaces, everything else is false     
+        Initialize the visibleArray, setting values to true for spaces, everything else is false
     */
     function initializeVisibleCharacterArray()
     {
-	var fullPuzzleString = getFullPuzzleString();
+    var fullPuzzleString = getFullPuzzleString();
         visibleArray = Array(fullPuzzleString.length)//make array same length as the puzzle String
         for(i=0;i<fullPuzzleString.length;i++)
         {
@@ -176,15 +176,15 @@ used to grab the base64 encoded value from the url of the page to create the puz
             }
         }
     }
-    
+
 /*Updates the global array visibleArray based on the guessedChar, which holds a boolean value for each character in the puzzle String,
 if  the value is true, the character will be displayed, if false, it will be a underscore
 This function also updates the hangman image on the page and lowers the tries counter if the guessedChar is not found in the puzzleString
 */
     function updateVisibleCharacterArray(guessedChar)
-    {   
-	var fullPuzzleString = getFullPuzzleString();
-                var used = isGuessUsed(guessedChar)//check guess against array, update 
+    {
+    var fullPuzzleString = getFullPuzzleString();
+                var used = isGuessUsed(guessedChar)//check guess against array, update
 
         var correctGuess = false;
         for(i=0;i<fullPuzzleString.length;i++)
@@ -195,16 +195,16 @@ This function also updates the hangman image on the page and lowers the tries co
                 //console.log(visibleArray)
                 correctGuess = true;
             }
-		}
+        }
             if(correctGuess == false)//no correct characters were found
             {
-                if(used == false)//character hasent been used                
+                if(used == false)//character hasent been used
                 {
                     triesCounter=triesCounter-1;
-		            document.getElementById("tries").src = "images/hangman"+triesCounter+".png"
+                    document.getElementById("tries").src = "images/hangman"+triesCounter+".png"
                 }
 }
-        
+
         //console.log(visibleArray)
     }
 
@@ -214,11 +214,11 @@ This function also updates the hangman image on the page and lowers the tries co
         is true then the character at index i of the puzzle string will be displayed or else it will be an underscore
     */
     function createDisplayString()
-    {    
-	var fullPuzzleString = getFullPuzzleString();
+    {
+    var fullPuzzleString = getFullPuzzleString();
         var displayString = fullPuzzleString;
            for(i=0;i<visibleArray.length;i++)
-            {   
+            {
                 if(visibleArray[i]==false)
                 {
                     displayString = displayString.substr(0, i) + '_' + displayString.substr(i + 1);
@@ -227,119 +227,119 @@ This function also updates the hangman image on the page and lowers the tries co
             }
             return displayString;
     }
-	
-	/*
-		takes the user input from the text box and validates it, if invalid, an error will be displayed
-		if the character is valid, the puzzle will be updated accordingly, letters will be revealed or a try
-		removed.
-	*/
-	function pressGuessButton()
-	{
-		var fullPuzzleString = getFullPuzzleString();
-		var guessString = document.getElementById("userGuess").value;
-		var cleanGuess = sanatizeGuess(guessString);
-		var isValid = validateGuess(cleanGuess);
-		if(isValid)
-		{
-			updateVisibleCharacterArray(cleanGuess)//update display  based on guess, updates tries
-			var disp = createDisplayString()
-			document.getElementById("header").innerHTML = disp;
-			warning = document.getElementById("guessWarning");
-			if (warning.className.indexOf("w3-hide") == -1)//If the warning is NOT hidden, then hide it
-			{
-				warning.className += " w3-hide";//hide the warning
-			}
-			document.getElementById("userGuess").value = ""//empty out the guess textbox
+
+    /*
+        takes the user input from the text box and validates it, if invalid, an error will be displayed
+        if the character is valid, the puzzle will be updated accordingly, letters will be revealed or a try
+        removed.
+    */
+    function pressGuessButton()
+    {
+        var fullPuzzleString = getFullPuzzleString();
+        var guessString = document.getElementById("userGuess").value;
+        var cleanGuess = sanatizeGuess(guessString);
+        var isValid = validateGuess(cleanGuess);
+        if(isValid)
+        {
+            updateVisibleCharacterArray(cleanGuess)//update display  based on guess, updates tries
+            var disp = createDisplayString()
+            document.getElementById("header").innerHTML = disp;
+            warning = document.getElementById("guessWarning");
+            if (warning.className.indexOf("w3-hide") == -1)//If the warning is NOT hidden, then hide it
+            {
+                warning.className += " w3-hide";//hide the warning
+            }
+            document.getElementById("userGuess").value = ""//empty out the guess textbox
             document.getElementById("letterList").innerHTML = createUsedCharList();
-			if(triesCounter == 0)//GAME OVER
-			{
-				gameOver();
-			}
-			//need to check if the game has been won
-			var didWin = didYouWin()
-			if(didWin)
-			{
-				youWin()
-			}
-				
-		}
-		else//guess is invalid
-		{	
-			warning = document.getElementById("guessWarning");
-			warning.className = warning.className.replace(" w3-hide", "");
-		}
-		
-	}
-	
-	/*Calls the pressGuessButton() function when enter is pressed on the guess textbox*/
-	function onEnterGuess(event)
-	{
-	    if (event.keyCode == 13)
-	    { 
-	        pressGuessButton();
-	    }
-	}
-	
-	/*trim the guess String and change to uppercase then return clean value*/
-	function sanatizeGuess(uncleanGuess)
-	{
-		uncleanGuess = uncleanGuess.toUpperCase();
-		uncleanGuess = uncleanGuess.trim();
-		return uncleanGuess;	
-	}
-	
-	/*
-		Guess should only be a single uppercase letter, true if so, false if not
-	*/
-	function validateGuess(cleanString)
-	{	
-		if(cleanString.length != 1)
-		{
-			return false;
-		}
-		else//only one character, need to make sure its a capital Letter
-		{ 
-			var regex=/[A-Z]/;//checks to see if the character is a letter
-			return cleanString.match(regex)
-		}
-	}
-	
-	function gameOver()
-	{
-		document.getElementById("modalHeader").innerHTML = "Game Over";
-		document.getElementById("modalText").innerHTML = "Great try, but you were not able to solve the puzzle";
-		showEndingModal()
-	}
-	
-	function youWin()
-	{
-		document.getElementById("modalHeader").innerHTML = "You Win!";
-		document.getElementById("modalText").innerHTML = "Great job, you are a puzzle master!";
-		showEndingModal();
-	}
-	
-	/*checks the status of the game by looping through the visibleArray, if there is any false values,
-	it means that there is still unrevealed letters and the game is still not over*/
-	function didYouWin()
-	{
-		var didWin = true
-		for(i=0;i<visibleArray.length;i++)
-		{
-			if(visibleArray[i] == false)//there are still blanks in the puzzle
-			{
-				didWin = false;
-			}
-		}
-		return didWin;
-	}
-	
-	//display modal
-	function showEndingModal()
-	{
-		
-		document.getElementById('endingModal').style.display='block';
-		document.getElementById('userGuess').blur();
-	}
+            if(triesCounter == 0)//GAME OVER
+            {
+                gameOver();
+            }
+            //need to check if the game has been won
+            var didWin = didYouWin()
+            if(didWin)
+            {
+                youWin()
+            }
+
+        }
+        else//guess is invalid
+        {
+            warning = document.getElementById("guessWarning");
+            warning.className = warning.className.replace(" w3-hide", "");
+        }
+
+    }
+
+    /*Calls the pressGuessButton() function when enter is pressed on the guess textbox*/
+    function onEnterGuess(event)
+    {
+        if (event.keyCode == 13)
+        {
+            pressGuessButton();
+        }
+    }
+
+    /*trim the guess String and change to uppercase then return clean value*/
+    function sanatizeGuess(uncleanGuess)
+    {
+        uncleanGuess = uncleanGuess.toUpperCase();
+        uncleanGuess = uncleanGuess.trim();
+        return uncleanGuess;
+    }
+
+    /*
+        Guess should only be a single uppercase letter, true if so, false if not
+    */
+    function validateGuess(cleanString)
+    {
+        if(cleanString.length != 1)
+        {
+            return false;
+        }
+        else//only one character, need to make sure its a capital Letter
+        {
+            var regex=/[A-Z]/;//checks to see if the character is a letter
+            return cleanString.match(regex)
+        }
+    }
+
+    function gameOver()
+    {
+        document.getElementById("modalHeader").innerHTML = "Game Over";
+        document.getElementById("modalText").innerHTML = "Great try, but you were not able to solve the puzzle";
+        showEndingModal()
+    }
+
+    function youWin()
+    {
+        document.getElementById("modalHeader").innerHTML = "You Win!";
+        document.getElementById("modalText").innerHTML = "Great job, you are a puzzle master!";
+        showEndingModal();
+    }
+
+    /*checks the status of the game by looping through the visibleArray, if there is any false values,
+    it means that there is still unrevealed letters and the game is still not over*/
+    function didYouWin()
+    {
+        var didWin = true
+        for(i=0;i<visibleArray.length;i++)
+        {
+            if(visibleArray[i] == false)//there are still blanks in the puzzle
+            {
+                didWin = false;
+            }
+        }
+        return didWin;
+    }
+
+    //display modal
+    function showEndingModal()
+    {
+
+        document.getElementById('endingModal').style.display='block';
+        document.getElementById('userGuess').blur();
+    }
 
 /*
 This function will check the guessed char against the list of other used characters
@@ -359,7 +359,7 @@ function isGuessUsed(guessedChar)
       {
         return true
       }
-   } 
+   }
        usedLetters.push(guessedChar)
         console.log(usedLetters)
         return false
@@ -375,9 +375,3 @@ function createUsedCharList()
     }
     return usedLetterString
 }
-
-
-	
-
-
-  
